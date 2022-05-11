be41628dfcf126bf5e262d77de7d2b5ccac9d11a
Maria Capurro <mariacapm@gmail.com>
2022-05-11 16:28:04 +0200

    Crea función validarNombre

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return "Este campo debe tener al menos 1 caracter";
+  }
+
+  if (nombre.length >= 50) {
+    return "Este campo debe tener menos de 50 caracteres";
+  }
+  return "";
+}
+
